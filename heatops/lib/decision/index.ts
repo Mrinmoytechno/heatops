e646112b59c94aeab0b979d010d19e923f17b67b
Mrinmoytechno <paulsonali149@gmail.com>
2026-08-19 19:14:35 +0530

    Create index.ts

---
+++ heatops/lib/decision/index.ts
@@ -0,0 +1,11 @@
+export {
+  calculateDecision,
+} from "./decision-engine";
+
+export type {
+  CandidateDecision,
+  DecisionActionType,
+  DecisionRecommendation,
+  DecisionTradeOff,
+  RecommendationPriority,
+} from "@/types/decision";
