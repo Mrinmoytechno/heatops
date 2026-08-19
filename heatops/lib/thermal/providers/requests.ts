2286d9853585e5e2396bf4f57bb75752027ba687
Mrinmoytechno <paulsonali149@gmail.com>
2026-08-19 14:25:54 +0530

    Update requests.ts

--- heatops/lib/thermal/providers/requests.ts
+++ heatops/lib/thermal/providers/requests.ts
@@ -4,21 +4,51 @@ export type CurrentConditionsRequest = {
 };
 
 export type ForecastRequest = {
+  siteId: string;
+
   latitude: number;
   longitude: number;
+
   startTime: string;
   endTime: string;
+
+  /**
+   * GeoJSON Polygon or FeatureCollection defining
+   * the site's area of interest.
+   *
+   * Coordinates use [longitude, latitude].
+   */
+  polygonAoi: GeoJsonFeatureCollection;
 };
 
 export type HeatmapRequest = {
   latitude: number;
   longitude: number;
+
   startTime: string;
   endTime: string;
+
+  polygonAoi: GeoJsonFeatureCollection;
 };
 
 export type EnvironmentalRequest = {
   latitude: number;
   longitude: number;
   timestamp: string;
+};
+
+export type GeoJsonFeatureCollection = {
+  type: "FeatureCollection";
+
+  features: Array<{
+    type: "Feature";
+
+    properties: Record<string, unknown>;
+
+    geometry: {
+      type: "Polygon";
+
+      coordinates: number[][][];
+    };
+  }>;
 };
