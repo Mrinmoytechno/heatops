e38345d6071dcef3ad737f3ea41b0d814373e558
Mrinmoytechno <paulsonali149@gmail.com>
2026-08-19 16:43:06 +0530

    Create index.ts

---
+++ heatops/lib/impact/index.ts
@@ -0,0 +1,11 @@
+export {
+  calculateImpact,
+} from "./impact-engine";
+
+export {
+  calculateSeverity,
+} from "./severity";
+
+export {
+  calculateProductiveHoursAtRisk,
+} from "./productivity";
